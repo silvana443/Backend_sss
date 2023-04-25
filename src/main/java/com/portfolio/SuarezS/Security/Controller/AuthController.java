0f376e2baf48d588b8808a3e99ea2bdde6c1500f
Silvana Soledad Suarez <suarezsilvana894@gmail.com>
2023-04-25 14:40:23 -0300

    modificacion  cross origins

--- src/main/java/com/portfolio/SuarezS/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/SuarezS/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin (origins="https://frontendsss.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
